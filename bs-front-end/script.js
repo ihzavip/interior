c2c08a1f138a5817e975a1df79d24e77fdfb6327
ihzavip <39728148+ihzavip@users.noreply.github.com>
2022-04-08 21:01:22 +0700

    it tooks me a whole day to finish the layout wtf
    do the interactivity next

---
+++ bs-front-end/script.js
@@ -0,0 +1,12 @@
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  }); 
